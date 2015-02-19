abc859684900a7e505a650ea4457a6548abf62d4
nithin mohan <nithinmohan94@gmail.com>
2015-02-19 13:32:37 -0800

    FileManager Added with CopyFolder: New Project function complete

---
+++ game/FileManager.h
@@ -0,0 +1,30 @@
+#ifndef _FileManager_
+#define _FileManager_
+#include <iostream>
+#include <cstdio>
+#include "dirent.h"
+#include <string.h>
+#include <Windows.h>
+#if defined _MSC_VER
+#include <direct.h>
+#elif defined __GNUC__
+#include <sys/types.h>
+#include <sys/stat.h>
+#endif
+class FileManager{
+public:
+	bool copyFile(const char* source,const char* dest);
+	bool copyFolder(const char* source,const char* dest);
+	void FileManager::createDir(const char* dir);
+	static FileManager* Instance(){
+		if(s_pInstance==0){
+			s_pInstance=new FileManager();
+		}
+		return s_pInstance;
+	}
+private:
+	FileManager(){};
+	static FileManager* s_pInstance;
+};
+typedef FileManager TheFileManager;
+#endif
--- game/main.cpp
+++ game/main.cpp
@@ -1,16 +1,28 @@
 #include <SDL.h>
 #include <stdio.h>
-
+#include <Windows.h>
+#include "FileManager.h"
+using namespace std;
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main( int argc, char* args[] )
 {
+	UINT32 frameStart,frameTime;
+	AllocConsole();
+	freopen("CON","w",stdout);
+
 	SDL_Window* window = NULL;
 	int running;
 	SDL_Surface* screenSurface = NULL;
 	SDL_Event event;
+	
+	TheFileManager::Instance()->copyFolder("fl","fl2");
 
+    //// clean and more secure
+    //// feof(FILE* stream) returns non-zero if the end of file indicator for stream is set
+
+   
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 	{
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
